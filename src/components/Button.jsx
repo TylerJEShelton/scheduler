df3220175186a54cf8942050c3c5bc12ce1edc7b
Tyler <tylershelton99@gmail.com>
2021-11-18 11:14:49 -0500

    Change component extensions as per my preference of jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,13 +6,6 @@ import "components/Button.scss";
 export default function Button(props) {
    let buttonClass = classNames("button", {"button--confirm": props.confirm}, {"button--danger": props.danger});
 
-   // if (props.confirm) {
-   //    buttonClass += " button--confirm";
-   // }
-   // if (props.danger) {
-   //    buttonClass += " button--danger";
-   // }   
-      
    return (
       <>
             <button disabled={props.disabled} onClick={props.onClick} className={buttonClass}>{props.children}</button>  
