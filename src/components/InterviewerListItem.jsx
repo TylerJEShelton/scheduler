ce5f99736bddf9257c3fd50298e241202813c10f
Tyler <tylershelton99@gmail.com>
2021-11-19 12:21:04 -0500

    Refactor InterviewerListItem component and the story tests so that InterviewerList handles the id of the interviewer, not the InterviewerListItem component

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -7,7 +7,7 @@ export default function InterviewerListItem (props) {
   let interviewerListItemClass = classNames("interviewers__item", {"interviewers__item--selected": props.selected})
 
   return (
-    <li className={interviewerListItemClass} onClick={() => props.setInterviewer(props.id)}>
+    <li className={interviewerListItemClass} onClick={props.setInterviewer}>
       <img 
         className={"interviewers__item-image"}
         src={props.avatar}
--- stories/index.js
+++ stories/index.js
@@ -100,7 +100,7 @@ storiesOf("DayList", module)
         id={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        setInterviewer={action("setInterviewer")}
+        setInterviewer={() => action("setInterviewer")(interviewer.id)}
       />
     ));
 
