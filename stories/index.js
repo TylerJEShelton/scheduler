d68f7af5ce933eab37e5e6490e5ed4ec485898be
Tyler <tylershelton99@gmail.com>
2021-11-19 16:43:45 -0500

    Create the Empty component and add a test to the Appointment storybook

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.jsx";
 import Header from "components/Appointment/Header.jsx";
+import Empty from "components/Appointment/Empty.jsx";
 
 storiesOf("Button", module)
   .addParameters({
@@ -149,5 +150,10 @@ storiesOf("Appointment", module)
   .add("Header", () => (
     <Header
       time="12pm"
+    />
+  ))
+  .add("Empty", () => (
+    <Empty
+      onAdd={action("onAdd")}
     />
   ));
