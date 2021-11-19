c15da46a19a0c2b8a9214d38cf1743abe1115aa1
Tyler <tylershelton99@gmail.com>
2021-11-19 17:05:03 -0500

    Create the Show component for the Appointment as well as the storybook test. Use data from props to populate the student and interviewer names.

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.jsx";
 import Header from "components/Appointment/Header.jsx";
 import Empty from "components/Appointment/Empty.jsx";
+import Show from "components/Appointment/Show.jsx";
 
 storiesOf("Button", module)
   .addParameters({
@@ -155,5 +156,13 @@ storiesOf("Appointment", module)
   .add("Empty", () => (
     <Empty
       onAdd={action("onAdd")}
+    />
+  ))
+  .add("Show", () => (
+    <Show
+      student={"Lydia Miller-Jones"}
+      interviewer={interviewer.name}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}
     />
   ));
