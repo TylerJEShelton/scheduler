88d284469ad044723c11f98bdeb06f5d26f6237e
Tyler <tylershelton99@gmail.com>
2021-11-19 17:14:57 -0500

    Create the Confirm component for Appointment as well as a storybook test.  Used data from props to provide the message and onClick functionality for both the cancel and confirm Buttons which were imported from the Button component

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment/index.jsx";
 import Header from "components/Appointment/Header.jsx";
 import Empty from "components/Appointment/Empty.jsx";
 import Show from "components/Appointment/Show.jsx";
+import Confirm from "components/Appointment/Confirm";
 
 storiesOf("Button", module)
   .addParameters({
@@ -164,5 +165,12 @@ storiesOf("Appointment", module)
       interviewer={interviewer.name}
       onEdit={action("onEdit")}
       onDelete={action("onDelete")}
+    />
+  ))
+  .add("Confirm", () => (
+    <Confirm
+      message="Delete the appointment"
+      onConfirm={action("onConfirm")}
+      onCancel={action("onCancel")}
     />
   ));
