34891b5679c168915069994ada05fd545db841c3
Tyler <tylershelton99@gmail.com>
2021-11-20 16:04:33 -0500

    Add helper selector function that will get the appointments for a specific day.

---
+++ src/helpers/selectors.js
@@ -0,0 +1,22 @@
+export function getAppointmentsForDay(state, day) {
+  const days = state.days;
+  let dayID = null;
+  let currentDayAppointments = [];
+  const appointments = [];
+
+  days.forEach(arrayDay => {
+    if (arrayDay.name === day) {
+      dayID = arrayDay.id;
+    }
+  });
+
+  if(days[dayID - 1]) {
+    currentDayAppointments = days[dayID - 1].appointments;
+  }
+
+  currentDayAppointments.forEach(appointmentID => {
+    appointments.push(state.appointments[appointmentID]);
+  });
+
+  return appointments;
+}
