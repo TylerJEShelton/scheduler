2428c5efbb246187509af0e4b86e81a2f81b6468
Tyler <tylershelton99@gmail.com>
2021-11-25 18:40:43 -0500

    Add comments to useApplicationData hook

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-
 // API link constants
 const daysURL = `api/days`;
 const appointmentsURL = `/api/appointments`;
 const interviewersURL = `/api/interviewers`;
 
 export default function useApplicationData() {
+  // declare state with initial values
   const [state, setState] = useState({
     day: "Monday",
     days: [],
@@ -15,13 +15,15 @@ export default function useApplicationData() {
     interviewers: {}
   });
 
+  // set the currently selected date in the sidebar
   const setDay = day => setState({...state, day});
   
   useEffect(() => {
+    // pull all of the API data and update state with the newly requested data
     Promise.all([
-      Promise.resolve(axios.get(daysURL)),
-      Promise.resolve(axios.get(appointmentsURL)),
-      Promise.resolve(axios.get(interviewersURL)),
+      axios.get(daysURL),
+      axios.get(appointmentsURL),
+      axios.get(interviewersURL),
     ]).then((all) => {
       setState(prev => ({
         ...prev,
@@ -32,15 +34,19 @@ export default function useApplicationData() {
     });
   }, []);
 
-
+  // records the newly created or edited appointment in the API
   function bookInterview(id, interview) {
     let spots = 0;
     let dayID = 0;
     let appointmentIDs = [];
+    
+    // create an appointment variable at the passed in id with the passed in interview data
     const appointment = {
       ...state.appointments[id],
       interview: { ...interview }
     };
+
+    // create a new appointments variable that replaces the appointment at the passed in id
     const appointments = {
       ...state.appointments,
       [id]: appointment
@@ -48,20 +54,27 @@ export default function useApplicationData() {
     
     return axios.put(`${appointmentsURL}/${id}`, {interview})
     .then(() => {
+      // after successfully updating the interview information for the specific id in the API, need to update the local state with the new information.
+
       for (const day of state.days) {
+        // loop through days in state to find the day.id and array of appointments for the day that contains the specified appointment id
         if (day.appointments.includes(id)){
           dayID = day.id;
           appointmentIDs = day.appointments;  
         }
       }
+      // loop through the array of appointment ids and add up the spots available for appointments with no interview information
       appointmentIDs.forEach(appointmentID => {
         if (!appointments[appointmentID].interview) {
           spots++;
         }
       });
+      // store the new spots remaining information in the day variable and a state.days information in the days variable
       const day = {...state.days[dayID - 1], spots: spots};
       const days = [...state.days];
+      // for the day with the specified id, update the day with the newly created day information
       days[dayID - 1] = day;
+      // set the state with the new appointments and days information
       setState({
         ...state,
         appointments,
@@ -70,34 +83,46 @@ export default function useApplicationData() {
     });      
   }
 
+  // deletes the specified appointment
   function cancelInterview(id) {
     let spots = 0;
     let dayID = 0;
     let appointmentIDs = [];
+
+    // create an appointment variable at the passed in id with null interview data
     const appointment = {
       ...state.appointments[id],
       interview: null
     };
+
+    // create a new appointments variable that replaces the appointment at the passed in id
     const appointments = {
       ...state.appointments,
       [id]: appointment
     };
-    return axios.delete(`${appointmentsURL}/${id}`, appointment)
+    return axios.delete(`${appointmentsURL}/${id}`)
       .then(() => {
+        // after successfully deleting the interview information for the specific id in the API, need to update the local state with the new information.
+        
         for (const day of state.days) {
+          // loop through days in state to find the day.id and array of appointments for the day that contains the specified appointment id
           if (day.appointments.includes(id)){
             dayID = day.id;
-            appointmentIDs = day.appointments;  
+            appointmentIDs = day.appointments;
           }
         }
+        // loop through the array of appointment ids and add up the spots available for appointments with no interview information
         appointmentIDs.forEach(appointmentID => {
           if (!appointments[appointmentID].interview) {
             spots++;
           }
         });
+        // store the new spots remaining information in the day variable and a state.days information in the days variable
         const day = {...state.days[dayID - 1], spots: spots};
         const days = [...state.days];
+        // for the day with the specified id, update the day with the newly created day information
         days[dayID - 1] = day;
+        // set the state with the new appointments and days information
         setState({
           ...state,
           appointments,
