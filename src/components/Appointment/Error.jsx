8d7f7ddf19b03a72a286f82651915a0eec09c3e0
Tyler <tylershelton99@gmail.com>
2021-11-25 22:29:05 -0500

    Add comment to Error component

--- src/components/Appointment/Error.jsx
+++ src/components/Appointment/Error.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+// Returns an error display that shows the error message passed through props
 export default function Error(props) {
   return(
     <main className="appointment__card appointment__card--error">
