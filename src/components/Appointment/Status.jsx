0dfc96c3524b92e2ee81f45ba4dfd58a88adfaf0
Tyler <tylershelton99@gmail.com>
2021-11-25 22:37:19 -0500

    Add comment to Status component

--- src/components/Appointment/Status.jsx
+++ src/components/Appointment/Status.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+// Return a loading screen when saving or deleting an appointment
 export default function Status(props) {
   return(
     <main className="appointment__card appointment__card--status">
