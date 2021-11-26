1f44f489e535bf5879618b7ce0a822ebe8b10d83
Tyler <tylershelton99@gmail.com>
2021-11-25 21:22:39 -0500

    Add comments to DayList component

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 import DayListItem from "./DayListItem";
 
+// Returns a DayListItem component for each day in the props.days array
 export default function DayList(props) {
-
   const displayDays = props.days.map((day) => {
     return(
       <DayListItem
