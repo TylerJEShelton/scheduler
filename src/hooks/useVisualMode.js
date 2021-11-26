9e05f550664cae30d2a255f49cc0f06603bc0fcf
Tyler <tylershelton99@gmail.com>
2021-11-25 20:17:19 -0500

    Add comments to useVisualMode hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,11 +4,13 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
+  // adds a mode to the history stack for the new transition, replace the previous one if replace is true
   const transition = (newMode, replace = false) => {
     setMode(newMode);
     replace ? setHistory(prev => [...prev.slice(0,-1), newMode]) : setHistory(prev => [...prev, newMode]);    
   };
 
+  // as long as the the history stack is longer than 1, move to the previous mode
   const back = () => {
     if (history.length === 1) {
       return;
