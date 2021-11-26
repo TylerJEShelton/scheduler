4db9aac26db889dfd3b470ed47077f2224006003
Tyler <tylershelton99@gmail.com>
2021-11-25 20:54:31 -0500

    Fixed a bug that caused an error if a new appointment was created without selecting an interviewer

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -75,7 +75,7 @@ export default function Appointment(props) {
       {mode === EDIT && (
         <Form
           student={props.interview.student}
-          interviewer={props.interview.interviewer.id}
+          interviewer={props.interview.interviewer ? props.interview.interviewer.id : null}
           interviewers={props.interviewers}
           onSave={save}
           onCancel={() => back()}
